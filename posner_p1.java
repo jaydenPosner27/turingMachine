6da9bd1d2a69397177bd6f35b4f34505d2962cd5
jaydenposner <jponser2@binghamton.edu>
2025-09-10 22:50:19 -0400

    Completed project maybe

--- posner_p1.java
+++ posner_p1.java
@@ -1,3 +1,4 @@
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -9,14 +10,15 @@ class State{
     List<Transition> transList;
     public State(String name){
         this.name = name;
+        transList = new ArrayList<>();
     }
 }
 class Transition {
-    char readChar;
-    int newState;
-    char change;
-    char direction;
-    public Transition(char readChar,int newState,char change,char direction){
+    String readChar;
+    String newState;
+    String change;
+    String direction;
+    public Transition(String readChar,String newState,String change,String direction){
         this.readChar = readChar;
         this.newState = newState;
         this.change = change;
@@ -24,32 +26,93 @@ class Transition {
     }
 }
 public class posner_p1{
-    public static void main(String [] args){
-Scanner scan = new Scanner(System.in);
-List<State> stateList = new ArrayList<State>();
+    public static void main(String [] args) throws FileNotFoundException{
+List<State> stateList = new ArrayList<>();
 String parts[];
-State start;
+State start = new State("-1");
+String textFile = args[0];
+String [] tape = args[1].split("");
+Scanner scan = new Scanner(new File(textFile));
+int tot=0;
 
-String line = scan.nextLine();
-
-while(line!=null){
+while(scan.hasNextLine()){
+    String line = scan.nextLine();
     parts=line.split("\\s+");
     if(parts[0].equals("state")){
         State newState = new State(parts[1]);
-        if(parts[2]!=null&&parts[2].equals("start")){
-            newState.start = true;
+        if(parts.length>2){
+            if(parts[2].equals("start")){
+                newState.start = true;
+            }
+            if(parts[2].equals("reject")){
+                newState.reject=true;
+                start = newState;
+            }
+            if(parts[2].equals("accept")){
+                newState.accept=true;
+            }
         }
-            stateList.add(new State(parts[1]));    
+            stateList.add(newState);    
+    }else if(parts[0].equals("transition")){
+        for(State state:stateList){
+            if(state.name.equals(parts[1])){
+                state.transList.add(new Transition(parts[2],parts[3],parts[4],parts[5]));
+            }
+}
     }
-    
-    line=scan.nextLine();
+}
+
+int index = 0;
+
+
+while(true){
+    for(Transition tran:start.transList){
+        if(tran.readChar.equals(tape[index])){
+            tot++;
+            tape[index]=tran.change;
+            index = switch(tran.direction){
+                case "L"->index--;
+                case "R"->index++;
+                default->index;
+            };
+            for(State changeState : stateList){
+                if(changeState.name.equals(tran.newState)){
+                    start = changeState;
+                }
+            }
+        }     
+  }
+   if(start.accept=true){
+            for(String tapeIndex:tape){
+            System.out.print(tapeIndex);
+            }
+            System.out.println(" accept");
+            break;
+        }
+        if(start.reject=true){
+            for(String tapeIndex:tape){
+            System.out.print(tapeIndex);
+            }
+            System.out.println(" reject");
+            break;
+        }
+        if(tot>Integer.parseInt(args[2])){
+            for(String tapeIndex:tape){
+            System.out.print(tapeIndex);
+            }
+            System.out.println(" quit");
+        }
+        if(index<0){
+            for(String tapeIndex:tape){
+            System.out.print(tapeIndex);
+            }
+            System.out.println(" crash");
+        } 
+
 }
 
 
 
 scan.close();
-
-
-
     }
 }
